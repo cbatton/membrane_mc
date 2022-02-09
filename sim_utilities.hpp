8e71c79d8cd52617603bb1c42cdf9ce6ff39d59a
Clay Batton <cbatton35@gmail.com>
2022-02-08 23:43:15 -0800

    Onto simulation as the last piece before extensive testing

--- sim_utilities.hpp
+++ sim_utilities.hpp
@@ -19,39 +19,17 @@ using namespace std;
 
 class SimUtilities {
     public:
-        double WrapDistance(double, double, double);
+        SimUtilities(MembraneMC*);
+        ~SimUtilities();
+        double WrapDistance(double, double);
         double LengthLink(int, int);
         void AreaNode(int);
         void NormalTriangle(int i, double normal[3]);
         void ShuffleSaru(Saru&, vector<int>&);
-
-        // Want to use one where I compute cotangents directly using cos, sin
-        double CosineAngle(int, int, int); // Will use a different scheme here.....
-        double CosineAngleNorm(int, int, int);
-        double Cotangent(double);
         double Cotangent(int, int, int);
-        void AcosFastInitialize();
-        inline double AcosFast(double);
-        void CotangentFastInitialize();
-        inline double CotangentFast(double);
 
-        // Look up table variables
-        const int look_up_density = 10001;
-        double acos_lu_min = -1.0;
-        double acos_lu_max = 1.0;
-        double cot_lu_min = 0.15;
-        double cot_lu_max = M_PI-0.15;
-        double lu_interval_acos = (acos_lu_max-acos_lu_min)/(look_up_density-1);
-        double lu_interval_cot = (cot_lu_max-cot_lu_min)/(look_up_density-1);
-        double in_lu_interval_acos = 1.0/lu_interval_acos;
-        double in_lu_interval_cot = 1.0/lu_interval_cot;
-
-        // Arrays to store look up tables
-        double lu_points_acos[look_up_density];
-        double lu_values_acos[look_up_density];
-        double lu_points_cot[look_up_density];
-        double lu_values_cot[look_up_density];
-
+        // MembraneMC pointer
+        MembraneMC* sys;
 };
 
 #endif
